a681651488a38cfd578bae4099478b13fd99753c
JoaoHPS06 <148792819+JoaoHPS06@users.noreply.github.com>
2025-08-29 21:51:43 -0300

    Delete ExtraClasse-main directory

--- ExtraClasse-main/src/tests/H08_test.py
+++
@@ -1,8 +0,0 @@
-"""
-Teste história de usuário H08
-
-História de usuário:
-    Como um professor, gostaria de ofertar vagas de iniciação científica no sistema
-"""
-
-
--- ExtraClasse-main/src/tests/H20_test.py
+++
@@ -1,6 +0,0 @@
-"""
-Teste história de usuário H20
-
-História de usuário:
-    Como um aluno, gostaria de me candidatar em um projeto de interesse
-"""
